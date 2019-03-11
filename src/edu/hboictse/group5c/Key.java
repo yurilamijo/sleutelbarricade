ce0f683c7a8a969b1037ade1993ba89c6d1b1885
alexsandy015 <44060744+alexsandy015@users.noreply.github.com>
2019-03-11 13:53:56 +0100

    Edit key class

---
+++ src/edu/hboictse/group5c/Key.java
@@ -0,0 +1,36 @@
+package edu.hboictse.group5c;
+
+public class Key {
+
+   private int posX;
+   private int posY;
+   private int Code;
+    //symbol moeten we nog uitzoeken
+
+    public Key(int posX, int posY, int Code){
+     this.posX = 0;
+     this.posY = 0;
+     this.Code = 0;
+    } // alles heeft als begin waarde 0
+
+ public void getCode(){
+
+ }
+
+ public void setCode(){
+
+ }
+
+ public void getPositionX(){
+
+ }
+
+ public void getPositionY(){
+
+ }
+
+
+
+
+
+}
