0da946728356cd9d37aec7a3fe334b03e753c0d8
NtheGamer <nielsvanschaik14@gmail.com>
2019-03-11 14:00:48 +0100

    added Tile.class

---
+++ src/edu/hboictse/group5c/Blocks/Tile.java
@@ -0,0 +1,24 @@
+package edu.hboictse.group5c.Blocks;
+
+import edu.hboictse.group5c.Key;
+
+public class Tile extends Block{
+    private Key key;
+
+    public Tile(int x, int y) {
+        super(x, y);
+    }
+
+    public Tile(int x, int y, Key key) {
+        super(x,y);
+        this.key = key;
+    }
+
+    public Key getKey() {
+        return key;
+    }
+
+    public void setKey(Key key) {
+        this.key = key;
+    }
+}
