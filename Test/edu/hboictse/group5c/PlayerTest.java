ec80130666502f0b8dd5e82eb4c8ef4b621eb8a5
Yuri Lamijo <yurilamijo@hotmail.com>
2019-03-11 14:22:29 +0100

    Add JUnit 4 Tests

---
+++ Test/edu/hboictse/group5c/PlayerTest.java
@@ -0,0 +1,33 @@
+package edu.hboictse.group5c;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+    Player player = new Player(2,3);
+
+    @Test
+    public void moveUp() {
+        player.moveUp();
+        assertEquals(4, player.getPositionY());
+    }
+
+    @Test
+    public void moveDown() {
+        player.moveDown();
+        assertEquals(2, player.getPositionY());
+    }
+
+    @Test
+    public void moveRight() {
+        player.moveRight();
+        assertEquals(3, player.getPositionX());
+    }
+
+    @Test
+    public void moveLeft() {
+        player.moveLeft();
+        assertEquals(1, player.getPositionX());
+    }
+}
