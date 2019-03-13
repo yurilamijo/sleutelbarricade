caab3da41c1df705374757970e7bf11e66a8b0e5
alexsandy015 <44060744+alexsandy015@users.noreply.github.com>
2019-03-13 14:10:03 +0100

    Update Key.java
    Key update

--- src/edu/hboictse/group5c/Key.java
+++ src/edu/hboictse/group5c/Key.java
@@ -2,35 +2,23 @@ package edu.hboictse.group5c;
 
 public class Key {
 
-   private int posX;
-   private int posY;
-   private int Code;
-    //symbol moeten we nog uitzoeken
 
-    public Key(int posX, int posY, int Code){
-     this.posX = 0;
-     this.posY = 0;
-     this.Code = 0;
+   private int Code;
+   //String symbol;
+   //symbol moeten we nog uitzoeken
+
+    public Key(int Code){
+        this.Code = Code;
     } // alles heeft als begin waarde 0
 
- public void getCode(){
 
+    public int getCode(){
+        return Code;
  }
 
- public void setCode(){
-
+ public void setCode(int Code){
+ this.Code = Code ;
  }
-
- public void getPositionX(){
-
- }
-
- public void getPositionY(){
-
- }
-
-
-
 
 
 }
