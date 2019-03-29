ed64829f95550e3b6baf002f6fbbfa5dee8743d1
Yuri Lamijo <yurilamijo@hotmail.com>
2019-03-29 13:46:14 +0100

    Added level logic

--- src/edu/hboictse/group5c/GameField/Level.java
+++ src/edu/hboictse/group5c/GameField/Level.java
@@ -3,6 +3,7 @@ package edu.hboictse.group5c.GameField;
 public class Level {
 
     private int[][] blocks;
+    private int levelNumber;
 
     /**
      * 0 -> Tile
@@ -17,18 +18,33 @@ public class Level {
      * 9 -> Player
      */
     public Level(int levelNumber) {
-        this.blocks = new int[][]{
-                {9, 1, 2, 0, 0, 0, 0, 2, 2, 2},
-                {0, 0, 0, 0, 5, 7, 0, 2, 2, 2},
-                {0, 0, 2, 0, 0, 0, 0, 4, 4, 5},
-                {0, 1, 2, 0, 0, 0, 0, 4, 3, 3},
-                {0, 1, 2, 2, 0, 0, 0, 3, 2, 4},
-                {0, 3, 2, 0, 0, 4, 0, 2, 4, 0},
-                {0, 1, 2, 4, 3, 2, 0, 0, 0, 0},
-                {6, 1, 2, 2, 0, 0, 0, 0, 0, 0},
-                {0, 1, 2, 2, 0, 0, 0, 0, 0, 0},
-                {0, 1, 2, 2, 0, 0, 0, 0, 0, 8}
-        };
+        this.levelNumber = levelNumber;
+        this.setLevel();
+    }
+
+    private void setLevel() {
+        switch (this.levelNumber) {
+            case 1:
+                this.blocks = new int[][]{
+                        {9, 1, 2, 0, 0, 0, 0, 2, 2, 2},
+                        {0, 0, 0, 0, 5, 7, 0, 2, 2, 2},
+                        {0, 0, 2, 0, 0, 0, 0, 4, 4, 5},
+                        {0, 1, 2, 0, 0, 0, 0, 4, 3, 3},
+                        {0, 1, 2, 2, 0, 0, 0, 3, 2, 4},
+                        {0, 3, 2, 0, 0, 4, 0, 2, 4, 0},
+                        {0, 1, 2, 4, 3, 2, 0, 0, 0, 0},
+                        {6, 1, 2, 2, 0, 0, 0, 0, 0, 0},
+                        {0, 1, 2, 2, 0, 0, 0, 0, 0, 0},
+                        {0, 1, 2, 2, 0, 0, 0, 0, 0, 8}
+                };
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void nextLevel() {
+        this.levelNumber += 1;
     }
 
     public int[][] getBlocks() {
