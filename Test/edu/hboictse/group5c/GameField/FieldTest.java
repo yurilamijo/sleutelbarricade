19676de4c8b8e497a5855893ae595768cb976316
Yuri Lamijo <yurilamijo@hotmail.com>
2019-04-01 12:41:45 +0200

    JUnit FieldTest

---
+++ Test/edu/hboictse/group5c/GameField/FieldTest.java
@@ -0,0 +1,29 @@
+package edu.hboictse.group5c.GameField;
+
+import edu.hboictse.group5c.Objects.Blocks.Block;
+import edu.hboictse.group5c.Objects.Player;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FieldTest {
+
+    private Block[][] blocks = new Block[10][10];
+
+    @Test
+    public void addBlocks() {
+    }
+
+    @Test
+    public void addPlayer() {
+        Player player = new Player(0,0);
+    }
+
+    @Test
+    public void movePlayer() {
+    }
+
+    @Test
+    public void setIcon() {
+    }
+}
